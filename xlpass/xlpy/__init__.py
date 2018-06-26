ab0d68641de2bfde5965903b160bb17fdef26906
kumpul4semut <38381895+kumpul4semut@users.noreply.github.com>
2018-06-26 13:44:25 +0000

    Create __init__.py

---
+++ xlpass/xlpy/__init__.py
@@ -0,0 +1,12 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Anggi Alberto'
+__version__         = '1.1.0'
+__author__          = 'Anggi Alberto'
+__editor__          = 'Mr.A_S'
+__author_email__    = 'albertoanggi@gmail.com'
+__editor_email__    = 'haijuga7@gmail.com'
+__url__             = 'http://github.com/albertoanggi/xl-py'
+
+__all__ = ['Config','XL']
